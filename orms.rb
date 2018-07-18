31320028078fe1b2a778aec9b8e39c8caa27f6b4
Jauke <joke@biloucorp.com>
2018-07-18 22:22:56 +0200

    v1.1.3

--- orms.rb
+++ orms.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #==============================================================================
-# ** OLD_RM_STYLE  v1.1.2
+# ** OLD_RM_STYLE  v1.1.3
 #------------------------------------------------------------------------------
 # By Joke @biloumaster <joke@biloucorp.com>
 # GitHub: https://github.com/RMEx/OLD_RM_STYLE
